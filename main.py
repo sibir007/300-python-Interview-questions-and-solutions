7f36496747bfd2058d8bb07b91ec18358014c8e5
sibir007 <sibriakoff2006@yandex.ru>
2025-01-15 15:59:06 +0300

    Исравил текст под md

---
+++ main.py
@@ -0,0 +1,24 @@
+import re
+
+match_header = r'(^\d+\. )(.*)'
+match_whitespace = r'^\s*$'
+
+with open('350interview.txt', 'rt', encoding='utf-8') as f:
+    with open('350interview_cor.txt', 'wt', encoding='utf-8') as f2:
+        counter = 1
+        while line:= f.readline():
+            
+            if(m:=re.match(match_whitespace, line)):
+                continue
+        
+            if m:=re.match(match_header, line):
+                f2.write(f'\n### {str(counter)}. {m.group(2).strip()}\n\n')
+                # print(m.group(2))
+                # print(f'### {str(counter)}. {m.group(2).strip()}')
+                counter += 1
+                continue
+            f2.write(line)
+            
+
+        
+        
